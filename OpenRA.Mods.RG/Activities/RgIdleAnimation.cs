935840975d4e6bfd4f16d77c6bddf410ee7fa551
geckosoft <tv2knet@gmail.com>
2010-10-26 18:01:30 +0200

    Added missing files

---
+++ OpenRA.Mods.RG/Activities/RgIdleAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Mods.Rg.Traits.Render;
+using OpenRA.Traits;
+using OpenRA.Traits.Activities;
+
+namespace OpenRA.Mods.Rg.Activities
+{
+	public class RgIdleAnimation : Idle
+	{
+		string sequence;
+		int delay;
+
+		public RgIdleAnimation(string sequence, int delay)
+		{
+			this.sequence = sequence;
+			this.delay = delay;
+		}
+
+		bool active = true;
+		public override IActivity Tick(Actor self)
+		{
+			if (!active) return NextActivity;
+
+			if (delay > 0 && --delay == 0)
+				self.Trait<RgRenderInfantry>().anim.PlayThen(sequence, () => active = false);
+
+			return this;
+		}
+
+		protected override bool OnCancel(Actor self)
+		{
+			active = false;
+			return true;
+		}
+
+	}
+}
---
+++ OpenRA.Mods.RG/Traits/RgIdleAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Rg.Traits
+{
+
+	class RgIdleAnimationInfo : ITraitInfo
+	{
+		public readonly int IdleWaitTicks = 50;
+		public readonly string[] Animations = { };
+		public object Create(ActorInitializer init) { return new RgIdleAnimation(this); }
+	}
+
+	// infantry prone behavior
+	class RgIdleAnimation : INotifyDamage, INotifyIdle
+	{
+		RgIdleAnimationInfo Info;
+		public RgIdleAnimation(RgIdleAnimationInfo info)
+		{
+			Info = info;
+		}
+
+		public void Damaged(Actor self, AttackInfo e)
+		{
+			if (self.GetCurrentActivity() is Activities.RgIdleAnimation)
+				self.CancelActivity();
+		}
+
+		public void Idle(Actor self)
+		{
+			self.QueueActivity(new Activities.RgIdleAnimation(Info.Animations.Random(self.World.SharedRandom),
+															Info.IdleWaitTicks));
+		}
+	}
+}
