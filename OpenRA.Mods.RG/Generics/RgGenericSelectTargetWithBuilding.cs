6df386741b5395280c417e2676414eed6aab3e1b
geckosoft <tv2knet@gmail.com>
2010-11-03 02:04:55 +0100

    Added: RgGenericSelectTargetWithBuilding.cs

---
+++ OpenRA.Mods.RG/Generics/RgGenericSelectTargetWithBuilding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Orders;
+
+namespace OpenRA.Mods.Rg.Generics
+{
+	public class RgGenericSelectTargetWithBuilding<T> : GenericSelectTarget
+	{
+		private Player Player = null;
+
+		public RgGenericSelectTargetWithBuilding(Player owner, Actor subject, string order, string cursor)
+			: base(subject, order, cursor)
+		{
+			Player = owner;
+		}
+
+		public override void Tick(World world)
+		{
+			var hasStructure = world.Queries.OwnedBy[Player]
+					.WithTrait<T>()
+					.Any();
+
+			if (!hasStructure)
+				world.CancelInputMode();
+		}
+	}
+}
