6cb2c210b83288114c0011bed53d4279cc003d01
geckosoft <tv2knet@gmail.com>
2010-11-09 20:37:23 +0100

    Added: Custom RGRemoveSelf activity (has some additional checks)

---
+++ OpenRg/Activities/RGRemoveSelf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA;
+using OpenRA.Traits;
+
+namespace OpenRg.Activities
+{
+	public class RGRemoveSelf : CancelableActivity
+	{
+		public override IActivity Tick(Actor self)
+		{
+			if (IsCanceled) return NextActivity;
+			self.World.AddFrameEndTask(a => { if (self.IsInWorld) self.World.Remove(self); if (!self.Destroyed) self.Destroy(); });
+			return null;
+		}
+	}
+}
