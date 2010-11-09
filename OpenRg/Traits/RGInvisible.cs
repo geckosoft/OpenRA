8b30b8c1eefc3d7a0a11be670cd8e8f15255ebd5
geckosoft <tv2knet@gmail.com>
2010-11-10 00:07:16 +0100

    Added: RGInvisible (Visible => false as default)

---
+++ OpenRg/Traits/RGInvisible.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA;
+using OpenRA.Traits;
+
+namespace OpenRg.Traits
+{
+	public class RGInvisibleInfo : ITraitInfo
+	{
+		public readonly bool Visible = false;
+
+		public object Create(ActorInitializer init)
+		{
+			return new RGInvisible(init.self, this);
+		}
+	}
+
+	public class RGInvisible : IRenderModifier, IVisibilityModifier
+	{
+		[Sync]
+		private bool _visible;
+
+		public RGInvisible(Actor self, RGInvisibleInfo info)
+		{
+			Visible = info.Visible;
+		}
+
+		public bool Visible
+		{
+			get { return _visible; }
+			set { _visible = value; }
+		}
+
+		public IEnumerable<Renderable> ModifyRender(Actor self, IEnumerable<Renderable> r)
+		{
+			if (Visible)
+				return r;
+
+			return new Renderable[] {};
+		}
+
+		public bool IsVisible(Actor self, Player byPlayer)
+		{
+			return Visible;
+		}
+	}
+}
