4713fb867d8b9083b80d5f78575ef1e237be5331
geckosoft <tv2knet@gmail.com>
2010-11-09 23:48:30 +0100

    Fixed: crate_spawn Added: A tooltip to crate_spawn (cant remove the 'tooltip' atm :/)

--- OpenRg/Traits/System/RGCrateSpawner.cs
+++ OpenRg/Traits/System/RGCrateSpawner.cs
@@ -54,5 +54,10 @@ namespace OpenRg.Traits.System
 					})));
 			return;
 		}
+
+		public IEnumerable<Renderable> ModifyRender(Actor self, IEnumerable<Renderable> r)
+		{
+			yield break;
+		}
 	}
 }
