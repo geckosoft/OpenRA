c009c52087213852a411eabacef14703150e8d96
geckosoft <tv2knet@gmail.com>
2010-11-14 09:29:02 +0100

    Fixed: OpenRg in general after rebase

--- OpenRg/Widgets/Delegates/RgMainMenuButtonsDelegate.cs
+++ OpenRg/Widgets/Delegates/RgMainMenuButtonsDelegate.cs
@@ -1,62 +1,80 @@
-using System.IO;
 using OpenRA;
 using OpenRA.FileFormats;
 using OpenRA.Server;
+using OpenRA.Network;
+using System.Collections.Generic;
 using OpenRA.Widgets;
 
 namespace OpenRg.Widgets.Delegates
 {
 	public class RGMainMenuButtonsDelegate : IWidgetDelegate
 	{
+		static bool FirstInit = true;
+
 		[ObjectCreator.UseCtor]
 		public RGMainMenuButtonsDelegate([ObjectCreator.ParamAttribute] Widget widget)
 		{
 			// Main menu is the default window
-			widget.GetWidget("MAINMENU_BUTTON_JOIN").OnMouseUp = mi =>
-			                                                     	{
-			                                                     		Widget.OpenWindow("JOINSERVER_BG");
-			                                                     		return true;
-			                                                     	};
-			widget.GetWidget("MAINMENU_BUTTON_CREATE").OnMouseUp = mi =>
-			                                                       	{
-			                                                       		Widget.OpenWindow("CREATESERVER_BG");
-			                                                       		return true;
-			                                                       	};
-			widget.GetWidget("MAINMENU_BUTTON_SETTINGS").OnMouseUp = mi =>
-			                                                         	{
-			                                                         		Widget.OpenWindow("SETTINGS_MENU");
-			                                                         		return true;
-			                                                         	};
-			// TODO Add music in? // widget.GetWidget( "MAINMENU_BUTTON_MUSIC" ).OnMouseUp = mi => { Widget.OpenWindow( "MUSIC_MENU" ); return true; };
-			widget.GetWidget("MAINMENU_BUTTON_QUIT").OnMouseUp = mi =>
-			                                                     	{
-			                                                     		Game.Exit();
-			                                                     		return true;
-			                                                     	};
+			widget.GetWidget("MAINMENU_BUTTON_JOIN").OnMouseUp = mi => { Widget.OpenWindow("JOINSERVER_BG"); return true; };
+			widget.GetWidget("MAINMENU_BUTTON_CREATE").OnMouseUp = mi => { Widget.OpenWindow("CREATESERVER_BG"); return true; };
+			widget.GetWidget("MAINMENU_BUTTON_SETTINGS").OnMouseUp = mi => { Widget.OpenWindow("SETTINGS_MENU"); return true; };
+			widget.GetWidget("MAINMENU_BUTTON_MUSIC").OnMouseUp = mi => { Widget.OpenWindow("MUSIC_MENU"); return true; };
+			widget.GetWidget("MAINMENU_BUTTON_QUIT").OnMouseUp = mi => { Game.Exit(); return true; };
 
 			var version = widget.GetWidget<LabelWidget>("VERSION_STRING");
 
 			if (FileSystem.Exists("VERSION"))
 			{
-				Stream s = FileSystem.Open("VERSION");
-				string versionFileContent = s.ReadAllText();
+				var s = FileSystem.Open("VERSION");
+				var versionFileContent = s.ReadAllText();
 				version.Text = versionFileContent;
 				s.Close();
 
 				MasterServerQuery.OnVersion += v =>
-				                               	{
-				                               		if (!string.IsNullOrEmpty(v))
-				                               			version.Text = versionFileContent + "\nLatest: " + v;
-				                               	};
+				{
+					if (!string.IsNullOrEmpty(v))
+						version.Text = versionFileContent + "\nLatest: " + v;
+				};
 				MasterServerQuery.GetCurrentVersion(Game.Settings.Server.MasterServer);
 			}
 			else
 			{
-				version.Text = "OpenRg: Development Build";
+				version.Text = "Dev Build";
 			}
 			MasterServerQuery.ClientVersion = version.Text;
 
 			MasterServerQuery.GetMOTD(Game.Settings.Server.MasterServer);
+
+			if (FirstInit)
+			{
+				FirstInit = false;
+				Game.ConnectionStateChanged += orderManager =>
+				{
+					Widget.CloseWindow();
+					switch (orderManager.Connection.ConnectionState)
+					{
+						case ConnectionState.PreConnecting:
+							Widget.OpenWindow("MAINMENU_BG");
+							break;
+						case ConnectionState.Connecting:
+							Widget.OpenWindow("CONNECTING_BG",
+								new Dictionary<string, object> { { "host", orderManager.Host }, { "port", orderManager.Port } });
+							break;
+						case ConnectionState.NotConnected:
+							Widget.OpenWindow("CONNECTION_FAILED_BG",
+								new Dictionary<string, object> { { "host", orderManager.Host }, { "port", orderManager.Port } });
+							break;
+						case ConnectionState.Connected:
+							var lobby = Widget.OpenWindow("SERVER_LOBBY", new Dictionary<string, object> { { "orderManager", orderManager } });
+							lobby.GetWidget<ChatDisplayWidget>("CHAT_DISPLAY").ClearChat();
+							lobby.GetWidget("CHANGEMAP_BUTTON").Visible = true;
+							lobby.GetWidget("LOCKTEAMS_CHECKBOX").Visible = true;
+							lobby.GetWidget("DISCONNECT_BUTTON").Visible = true;
+							//r.GetWidget("INGAME_ROOT").GetWidget<ChatDisplayWidget>("CHAT_DISPLAY").ClearChat();	
+							break;
+					}
+				};
+			}
 		}
 	}
 }
